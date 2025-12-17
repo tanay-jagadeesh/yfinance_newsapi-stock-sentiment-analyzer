380c32136501290a6fb31bc03b43ba7cf97dda72
Tanay <tanayj2107@gmail.com>
2025-12-17 12:37:29 -0500

    got the daily data (open,close,high,low,volume)

--- stock_data.py
+++ stock_data.py
@@ -2,8 +2,12 @@ import pandas as pd
 import yfinance as yf
 from news_headline_scraper import ticker_symbols 
 
+start_date = '2024-01-01'
+end_date = '2025-01-01'
 
 def stock_prices():
 
     for ticker in ticker_symbols:
         ticker = yf.Ticker(ticker)
+        data = yf.download(ticker, start = start_date, end = end_date, interval = '1d')
+
