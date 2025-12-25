04b4c7e811b9fed48bcde42bc7151151814099a3
Tanay <tanayj2107@gmail.com>
2025-12-25 00:59:36 -0500

    did decision tree model and random forest model

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 # Load train/val/test sets
 X_train = pd.read_csv('X_train.csv')
@@ -18,3 +20,13 @@ reg_model.fit(X_train, y_train)
 
 #predictions
 reg_model_predictions = reg_model.predict(X_val)
+
+# Decision Tree Model
+dt_model = DecisionTreeRegressor()
+dt_model.fit(X_train, y_train)
+dt_predictions = dt_model.predict(X_val)
+
+# Random Forest Model
+rf_model = RandomForestRegressor()
+rf_model.fit(X_train, y_train)
+rf_predictions = rf_model.predict(X_val)
