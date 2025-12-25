ea9d14c4a65d233184ae301796bdc03a1924e6a4
Tanay <tanayj2107@gmail.com>
2025-12-25 00:16:43 -0500

    calculate len of df then did some train and val stuff

--- model_prep.py
+++ model_prep.py
@@ -12,3 +12,12 @@ df['target'] = df['next_day_change']
 matrix_x = df.drop(columns = ['ticker', 'date', 'date_dt', 'target', 'next_day_change'])
 
 matrix_y = df['target']
+
+#splitting data
+#train 70%, validation 15% and then test 15% (old, middle, new)
+
+n = len(df)
+
+train_end = int(n * 0.70)
+
+val_end = int(n * 0.85) #NOTE: where it ends/automatically goes to test end
