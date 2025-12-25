5233a7349fefd85171244d9ac883f320be57243c
Tanay <tanayj2107@gmail.com>
2025-12-25 00:12:26 -0500

    created x and y matrices respectfully

--- model_prep.py
+++ model_prep.py
@@ -1,6 +1,14 @@
 import pandas as pd
 
 df = pd.read_csv('features_scaled.csv')
+print(df.columns)
 
 #defined target variable
 df['target'] = df['next_day_change']
+
+#creating feature matrix X
+#starting by dropping columns that are not needed for the prediction
+
+matrix_x = df.drop(columns = ['ticker', 'date', 'date_dt', 'target', 'next_day_change'])
+
+matrix_y = df['target']
